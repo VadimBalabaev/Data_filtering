106a76e3c3153a3809f17138728811c2d71d89ea
Vadim Balabaev <mr.respect.vadb@gmail.com>
2025-07-15 14:28:40 +0300

    add age-based user filtering

--- app.js
+++ app.js
@@ -4,50 +4,87 @@ const container = document.getElementById('container');
 const userListBtn = document.getElementById('getUsersList');
 const list = document.createElement('ul');
 
-let userList = false;
-let ageBtn;
-let sortAgeU = true;
+let isUserList = false;
+let isSortAgeUsers = true;
+let isAllUsers = true;
 
-function createUser (element) {
-    const li = document.createElement('li');
-    li.textContent = `${element.name} - ${element.age}`;
-    list.appendChild(li);
+let ageBtn;
+let sortBtn;
+
+let newArrUsers = [...arrUsers];
+let filteredUserList = [];
+
+function createUser(element) {
+    if (!element) {
+        throw new Error('Не передан элемент');
+    } else {
+        const li = document.createElement('li');
+        li.textContent = `${element.name} - ${element.age}`;
+        list.appendChild(li);
+    }
 }
 
 userListBtn.addEventListener('click', () => {
-    if (!userList) {
+    if (!isUserList) {
         list.innerHTML = '';
-        arrUsers.forEach(user => {
-            createUser(user);
-        });
+        arrUsers.forEach(user => createUser(user));
+        isAllUsers = true;
     }
 
-    if(!ageBtn){
+    if (!ageBtn) {
         ageBtn = document.createElement('button');
         ageBtn.textContent = 'Старше 30';
 
         ageBtn.addEventListener('click', () => {
             list.innerHTML = '';
 
-            const sortUsers = [...arrUsers]
-            .filter(user => user.age >=30)
-            .sort((a, b) => sortAgeU ? a.age - b.age : b.age - a.age);
+            filteredUserList = newArrUsers
+                .filter(user => user.age >= 30);
 
-            sortUsers.forEach(user => createUser(user));
 
-            sortAgeU = !sortAgeU;
+            filteredUserList.forEach(user => createUser(user));
 
-            ageBtn.textContent = sortAgeU ? 'Сортировать по возрастаню ↑' : 'Сортировать по убыванию ↓';
-
-            userList = false;
+            isUserList = false;
+            isAllUsers = false;
         });
-    } else {
-        ageBtn.textContent = 'Старше 30';
-        sortAgeU = true;
     }
-    
+
+    if (!sortBtn) {
+        sortBtn = document.createElement('button');
+        sortBtn.textContent = 'Сортировать по возрасту';
+
+        sortBtn.addEventListener('click', () => {
+            list.innerHTML = '';
+
+            function filter(arr) {
+                arr = arr
+                    .sort((a, b) => isSortAgeUsers ? a.age - b.age : b.age - a.age);
+                arr.forEach(user => createUser(user));
+            }
+
+            switch (isAllUsers) {
+                case true:
+                    filter(newArrUsers);
+                    break;
+                case false:
+                    filter(filteredUserList);
+                    break;
+                default:
+                    console.warn('Флаг isAllUsers имеет неизвестное значение');
+                    break;
+            }
+
+            isSortAgeUsers = !isSortAgeUsers;
+
+            sortBtn.textContent = isSortAgeUsers ? 'Сортировать по возрастаню ↑' : 'Сортировать по убыванию ↓';
+
+            isUserList = false;
+        });
+    }
+
     container.appendChild(ageBtn);
+    container.appendChild(sortBtn);
     container.appendChild(list);
 
-    userList = true;
+    isUserList = true;
 });
